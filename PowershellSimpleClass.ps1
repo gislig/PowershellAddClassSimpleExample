a1d478fe9ca17255b89af99966de8e1900635ee7
Gisli Gudmundsson <gisli@iteggs.com>
2018-05-10 08:53:51 +0000

    First Example Commit

---
+++ PowershellSimpleClass.ps1
@@ -0,0 +1,21 @@
+﻿Class Fruit {
+    #Set the variable
+    [string]$FruitName
+
+    #Create the constructor
+    Fruit ($FruitName) {
+        $this.FruitName = $FruitName
+    }
+}
+
+#Initialize the class
+[Fruit]$NewFruit
+
+#Get the fruit data, might be a sql data
+$Fruits = "Banana", "Apple", "Orange"
+
+#Write the fruit data to the class
+$Fruits | % { $NewFruit.FruitName = $_ }
+
+#Writing the fruits to console or do something
+$Fruits
